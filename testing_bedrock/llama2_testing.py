ba705effac562038feec63d99d2a525e5adb1617
vaibhav <bomlevaihav@gmail.com>
2024-10-13 15:08:06 +0530

    bedreock invoke test

--- testing_bedrock/llama2_testing.py
+++ testing_bedrock/llama2_testing.py
@@ -3,3 +3,16 @@ import json
 import boto3
 import sys
 
+bedrock = boto3.client("bedrock-runtime")
+
+model_id="meta.llama2-70b-chat-v1"
+
+response = bedrock.invoke_model(
+    modelId=model_id,
+    accept="application/json",
+    contentType="application/json"
+)
+
+response_body = json.loads(response.get("body").read())
+response_text = response_body["generation"]
+print(response_text)
